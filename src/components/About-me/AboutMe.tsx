b6ccdd1b2e3d5ec1ee6179c1298ab30db3002418
Andrés <andrescf@outlook.com>
2025-04-03 21:41:54 -0500

    Added strong style in words in the description

--- src/components/About-me/AboutMe.tsx
+++ src/components/About-me/AboutMe.tsx
@@ -10,14 +10,22 @@ export const AboutMe = () => {
           <section className={styles._content}>
             <p className={styles._title}>ABOUT ME</p>
             <p className={styles._description}>
-              My name is Andrés Cifuentes, a mechanical engineer who, due to my
-              enormous curiosity about the world of technology and facing new
-              challenges during my years as an engineering student, discovered
-              the world of programming and, in turn, the world of web
-              development. I have knowledge in full-stack development (HTML,
-              CSS, React, next.js, node.js, SQL, among others) and certified
-              studies by the Universidad Distrital Francisco José de Caldas and
-              Platzi in Full-Stack Web Development.
+              My name is <strong>Andrés Cifuentes</strong>, a{" "}
+              <strong>mechanical engineer</strong> who, due to my enormous{" "}
+              <strong>curiosity</strong> about the world of{" "}
+              <strong>technology</strong> and facing new{" "}
+              <strong>challenges</strong> during my years as an{" "}
+              <strong>engineering student</strong>, discovered the world of{" "}
+              <strong>programming</strong> and, in turn, the world of{" "}
+              <strong>web development</strong>. I have knowledge in{" "}
+              <strong>full-stack development</strong> (<strong>HTML</strong>,{" "}
+              <strong>CSS</strong>, <strong>React</strong>,{" "}
+              <strong>Next.js</strong>, <strong>Node.js</strong>,{" "}
+              <strong>SQL</strong>, among others) and{" "}
+              <strong>certified studies</strong> by the{" "}
+              <strong>Universidad Distrital Francisco José de Caldas</strong>{" "}
+              and <strong>Platzi</strong> in{" "}
+              <strong>Full-Stack Web Development</strong>.
             </p>
           </section>
           <section>
